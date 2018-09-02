791b635e170314b84f8045bef81a73956b6f06c9
apersonlikesc <1194261806@qq.com>
2018-09-02 19:04:22 +0800

    2018年 09月 02日 星期日 19:04:22 CST

--- spider/scrapy/naojingjizhuanwan/naojingjizhuanwan/spiders/zhiliti.py
+++ spider/scrapy/naojingjizhuanwan/naojingjizhuanwan/spiders/zhiliti.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 
 class ZhilitiSpider(scrapy.Spider):
     name = 'zhiliti'
-    allowed_domains = ['zhiliti.com.cn/html/jizhuanwan/list3_1.html']
-    start_urls = ['http://zhiliti.com.cn/html/jizhuanwan/list3_1.html/']
+    start_urls = ['http://www.zhiliti.com.cn/html/jizhuanwan/']
 
     def parse(self, response):
-        pass
+        for part in response.css("li"):
+            if(part.css("span::attr(onclick)").extract_first() == None):
+                continue
+
+            yield {
+                'description': part.css("h2 > a::text").extract(),
+                'answer': part.css("span::attr(onclick)").extract_first().replace("javascript:innerText=(","").replace(");","")
+            }
+        next_page_url = response.css("div.pagebar > a.next_page::attr(href)").extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
